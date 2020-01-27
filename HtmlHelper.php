1decc4450eb41345a1f54b3f7661e29941fbf81b
phptheme <dev@getphptheme.com>
2020-01-27 15:54:00 +0300

    n/a

---
+++ HtmlHelper.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author PhpTheme Dev Team <dev@getphptheme.com>
+ * @license MIT
+ * @link http://getphptheme.com
+ */
+namespace PhpTheme\HtmlHelper;
+
+class HtmlHelper extends BaseHtmlHelper
+{
+}
